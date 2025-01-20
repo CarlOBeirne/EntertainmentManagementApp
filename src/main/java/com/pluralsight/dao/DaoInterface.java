bcb478dc4a3c81f1b655a88d04b12666c2bb86b7
Carl OBeirne <carl.obeirne@expeditors.com>
2025-01-20 07:45:21 +0000

    Updating dao interface for save method to return optional

--- src/main/java/com/pluralsight/dao/DaoInterface.java
+++ src/main/java/com/pluralsight/dao/DaoInterface.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface DaoInterface<T> {
-    T save(T t);
+    Optional<T> save(T t);
     Optional<T> getById(int id);
     List<T> getAll();
     void delete(int id);
